b140f80fadec7c9fb89233941c65ce8a17c17c4a
prasanth-cg <prasanth.pedaprolu@capgemini.com>
2024-08-30 17:51:00 +0530

    added end point for - Retrive all orders assigned to a specific driver

--- src/main/java/com/fds/service/DeliveryDriversService.java
+++ src/main/java/com/fds/service/DeliveryDriversService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.fds.model.DeliveryDrivers;
+import com.fds.model.Orders;
 import com.fds.repository.DeliveryDriversRepository;
 
 import lombok.AllArgsConstructor;
@@ -21,5 +22,10 @@ public class DeliveryDriversService {
     
     public DeliveryDrivers getDriverById(int id) {
     	return deliveryDriversRepository.findById(id).get();
+    }
+    
+    public List<Orders> getAllOrdersByDriverId(int driverId){
+    	DeliveryDrivers driver = getDriverById(driverId);
+    	return driver.getOrders();
     }
 }
