eb7294d0f8f3bfafd145669bb01a5498519c069c
Siddique Baig <gayazuddin.siddique-baig@capgemini.com>
2024-09-01 09:06:48 +0530

    added OrdersDTO for specific order api

--- src/main/java/com/fds/model/Orders.java
+++ src/main/java/com/fds/model/Orders.java
@@ -1,6 +1,6 @@
 package com.fds.model;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
@@ -10,14 +10,11 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ForeignKey;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -33,9 +30,8 @@ public class Orders {
 	@Column(name="order_id")
 	private int order_id;
 	
-	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name="order_date", columnDefinition="DATETIME")
-	private Date order_date;
+	private LocalDateTime order_date;
 	
 	@Column(name="order_status", length=50)
 	private String order_status;
